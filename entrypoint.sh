2eaa690e5e64c15852cd3a11b59599b24e257abc
MrGazdag <44264503+MrGazdag@users.noreply.github.com>
2025-02-17 15:55:52 +0100

    make docker start in user-specific conditions

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
-dockerd
+dockerd --host=unix:///home/container/.docker/docker.sock --data-root /home/container/.docker/data --storage-driver=vfs
 
 # Run the Server
 eval ${MODIFIED_STARTUP}
