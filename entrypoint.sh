8deff889a082eef245e4c60a8e5def69fa2c453d
MrGazdag <44264503+MrGazdag@users.noreply.github.com>
2025-02-17 15:47:40 +0100

    start docker in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,7 @@ export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
+dockerd
 
 # Run the Server
 eval ${MODIFIED_STARTUP}
