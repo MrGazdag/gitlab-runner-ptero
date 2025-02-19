641b5a384a994e579253a35af0013e6dfd5d8dfa
MrGazdag <44264503+MrGazdag@users.noreply.github.com>
2025-02-19 08:09:36 +0100

    Remove sudo, run with elevated permissions

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,13 @@ cd /home/container
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
+# Start Docker
+echo "Starting Docker..."
+dockerd
+
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
-sudo dockerd --host=unix:///home/container/.docker/docker.sock --data-root /home/container/.docker/data --storage-driver=vfs
 
 # Run the Server
 eval ${MODIFIED_STARTUP}
